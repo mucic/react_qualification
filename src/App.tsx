45f4688f3034f6f6afae4cc9783d188ca13df2d1
Nenad Mucic <nenad.mucic@faxi.co.uk>
2020-04-16 14:13:45 +0200

    making it pritty - this can get reverted back to previoust

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,9 @@ function App() {
   // Get/Set state for repos
   const [repos, setRepos] = useState<GitData>([]);
   // Get/Set state for user input
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState<string>('');
 
+  // Component DID MOUNT
   useEffect(()=>{
     getRepos().then((data: GitData)=>{
       // console.log(data);
@@ -36,17 +37,36 @@ function App() {
   },[]);
 
   return (
-    <div className="App">
-      <input type="text" onChange={ e => setFilter(e.target.value) } value={filter} />
-      <ul>
-        {repos
-          .filter((repo: any) => repo.name.includes(filter))
-          .map((repo: GitDataStruct)=>(
-            <li key={repo.id}>
-              <a href={repo.html_url}>{repo.name}</a>
-            </li>
-          ))}
-      </ul>
+    <div className="App container">
+      <section className="showcase">
+        <section className="topic">
+          <h2><a href="#">#</a>React Qualification</h2>
+          <p>This is React qualification test mentored by Jonjoe!</p>
+        </section>
+        <section className="nes-container with-title">
+          <h3 className="title">My Git Repos</h3>
+          <div className="item">
+
+            <div className="nes-field is-inline">
+              <label htmlFor="inline_field">Filter:</label>
+              <input type="text" id="inline_field" className="nes-input" placeholder="Search" onChange={e => setFilter(e.target.value)} value={filter} />
+            </div>
+
+            <ul className="nes-list is-circle">
+              {repos
+                .filter((repo: GitDataStruct) => repo.name.includes(filter))
+                .map((repo: GitDataStruct) => (
+                  <li key={repo.id}>
+                    <a href={repo.html_url}>{repo.name}</a>
+                  </li>
+                ))}
+            </ul>
+
+          </div>
+
+
+        </section>
+      </section>
     </div>
   );
 }
